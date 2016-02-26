2f913e3b7b4cb101fe0f5c1ef7b1cdb3da9e7592
Todd Kenna <i.toddicus@gmail.com>
2016-02-26 11:19:22 -0600

    Starting getting the class working

--- BrewCLI.rb
+++ BrewCLI.rb
@@ -1,2 +1,22 @@
 require 'httparty'
 require 'pry'
+
+class Brewery
+
+def initialize(brewery)
+@brewery = brewery
+#@style = style
+#@beer = beer
+end
+
+def find_brewery
+	response = HTTParty.get("http://api.brewerydb.com/v2/search?q=#{@brewery}&type=brewery&key=181c714e2e3b0592a86e514652c5e727")
+	our_brewery1 = response
+	#our_brewery = response["data"]["name"]
+	binding.pry
+	#puts our_brewery
+end
+
+end
+
+brewery = Brewery.new("trve").find_brewery
